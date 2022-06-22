86a2c34c9325b9b0a5351d6d15d50644d7d3cfc0
Maxi-raph <102929251+Maxi-raph@users.noreply.github.com>
2022-06-22 04:15:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const desc = document.write("Writing a javascript code that prints out your name, height, and country on the screen<br><br>")
 const a = document.write("Raphael<br><br>")
 const b = document.write(" 6ft<br><br>")
 const c = document.write(" Nigeria")
